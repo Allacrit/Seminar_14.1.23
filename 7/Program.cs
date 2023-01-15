83958046b1843069849999665f176693499e6e6d
Artem Oleynikov <allacrit@yandex.ru>
2023-01-15 17:40:31 +0300

    add 7

---
+++ 7/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Clear();
+Console.WriteLine("Показать наибольшую цифру введённого числа на отрезке от 10 до 99.");
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int max = 0;
+if(number < 10 && number > 99)
+{
+    Console.WriteLine($"Число {number} не входит в диапазон данного отрезка");
+}
+else
+{
+    int digitOne = number / 10;
+    int digitTwo = number % 10;
+    if (digitOne > digitTwo) 
+        {
+        max = digitOne;
+        }
+    else
+        {
+        max = digitTwo;
+        }
+    Console.WriteLine($"В числе {number} наибольшая цифра {max}.");
+}
+
