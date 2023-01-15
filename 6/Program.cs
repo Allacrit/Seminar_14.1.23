304043bc96146388c00332db7530fa075c919d2f
Artem Oleynikov <allacrit@yandex.ru>
2023-01-15 17:18:53 +0300

    add 6

---
+++ 6/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.Write("Введите трёхзначное число: ");
+int number = int.Parse(Console.ReadLine());
+
+if(number < 1000 && number > 99)
+{
+Console.WriteLine($"Второй цифрой числа {number} является цифра {number / 10 % 10}.");
+}
+else
+{
+Console.WriteLine($"Число {number} не является ТРЁХЗНАЧНЫМ!!");
+}
