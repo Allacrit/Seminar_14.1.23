d3b364e0782faf6bed25e2d5dd258a2d989c819f
Artem Oleynikov <allacrit@yandex.ru>
2023-01-15 19:23:16 +0300

    add 10

---
+++ 10/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.WriteLine("Найти третью цифру числа или сообщить, что её нет");
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+//Console.WriteLine();
+//Console.WriteLine($"Дано число: {number}");
+
+if ((number / 100) > 0)
+{
+    Console.WriteLine($"Третьей цифрой числа {number} является цифра {number / 100 % 10}.");
+} 
+else
+{
+    Console.WriteLine($"У числа {number} нет третьей цифры.");
+}
