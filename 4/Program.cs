ade36c7bbc9d8a9db03b2d2740056e03121faf10
Artem Oleynikov <allacrit@yandex.ru>
2023-01-15 17:02:10 +0300

    add 4

---
+++ 4/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+string stringNumber = Console.ReadLine();
+int endNumber = int.Parse(stringNumber);
+int number = 0;
+
+Console.WriteLine($"Чётные числа из диапазона от 1 до {endNumber}:");
+
+while (number < endNumber)
+{
+    number++;
+    if (number % 2 == 0) Console.Write($"{number} ");
+}
