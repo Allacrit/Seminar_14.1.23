98edd24bb6498470e93e704c2f1e403256dd5520
Artem Oleynikov <allacrit@yandex.ru>
2023-01-15 16:54:17 +0300

    add 3

---
+++ 3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+
+Console.Write("Введите число: ");
+string writeNumber = Console.ReadLine();
+int number = int.Parse(writeNumber);
+string result = $"Число {number} является чётным!";
+
+if (number % 2 != 0) result = result.Replace("является", "не является");
+
+Console.WriteLine(result);
