e50f3ba3093d96b0203e027f096d8139af2f4085
Artem Oleynikov <allacrit@yandex.ru>
2023-01-15 19:04:18 +0300

    add 8

---
+++ 8/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Clear();
+Console.WriteLine("Удаление второго числа в трёхзначном числе.");
+Console.Write("Введите трёхзначное число: ");
+
+int number = int.Parse(Console.ReadLine());
+int newNumber = 0;
+
+if(number < 100 || number > 999)
+{
+    Console.WriteLine($"Число {number} не является ТРЁХЗНАЧНЫМ!");
+}
+else
+{
+NumberWithoutSecDig();
+Console.WriteLine($"Если из числа {number} удалить вторую цифру, получится число {newNumber}.");
+}
+
+int NumberWithoutSecDig()
+{
+    newNumber = number / 100 * 10 + number % 10;
+    return newNumber;
+}
